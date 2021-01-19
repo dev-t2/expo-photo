552440f57cd3bd6e0cc8fea8d25e3c613a5f0c6a
t2 <oo_ri@naver.com>
2021-01-20 02:52:11 +0900

    Apollo Client Setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import client from './apollo';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
