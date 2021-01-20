d5d2211537cdd701ec61cf68b492f70f64d3c940
t2 <oo_ri@naver.com>
2021-01-21 02:03:17 +0900

    Update GraphQL Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,34 @@
 import { memo } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 const Home = () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+
+  console.log(loading, data);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (data?.movies) {
+    return (
+      <div>
+        {data.movies.map((movie) => (
+          <h1 key={movie.id}>{movie.id}</h1>
+        ))}
+      </div>
+    );
+  }
+
   return <div>Home</div>;
 };
 
