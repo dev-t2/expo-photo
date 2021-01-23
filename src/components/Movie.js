81430508b0b8ec52f00b66e72ecb15e8b338416e
t2 <oo_ri@naver.com>
2021-01-23 16:24:09 +0900

    CSS Style Update

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,31 @@
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Movie = ({ id }) => {
+const Container = styled.div`
+  height: 400px;
+  border-radius: 8px;
+  width: 100%;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background-color: transparent;
+`;
+
+const Poster = styled.div`
+  background-image: url(${(props) => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 8px;
+`;
+
+const Movie = ({ id, bg }) => {
   return (
-    <div>
-      <Link to={`/${id}`}>{id}</Link>
-    </div>
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+    </Container>
   );
 };
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,52 @@
 import { memo } from 'react';
 import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 66px;
+  margin-bottom: 16px;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 36px;
+  margin-bottom: 10px;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
-      id
       title
       rating
       description_intro
@@ -22,9 +63,18 @@ const Detail = () => {
     variables: { id: parseInt(id) },
   });
 
-  console.log(loading, data);
-
-  return <div>Detail</div>;
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? 'Loading...' : `${data.movie.title} `}</Title>
+        <SubTitle>
+          {data?.movie?.language} / {data?.movie?.rating}
+        </SubTitle>
+        <Description>{data?.movie?.description_intro}</Description>
+      </Column>
+      <Poster bg={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 };
 
 export default memo(Detail);
